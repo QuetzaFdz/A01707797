00ca2d85ddf462c39fca5da97acc0334837869a8
QuetzaFdz <131408383+QuetzaFdz@users.noreply.github.com>
2023-06-05 19:24:56 -0600

    Update evidencia_final_modulo6_a01707797.py

--- evidencia_final_modulo6_a01707797.py
+++ evidencia_final_modulo6_a01707797.py
@@ -12,6 +12,6 @@ import pandas as pd
 st.title('Police Incident Reports from 2018 to 2020 in San Francisco')
 df=pd.read_csv('https://drive.google.com/file/d/1XjclXr7-4E3WXmtpQN_7HZboRM_a8RuU/view?usp=sharing/Police_Department_Incident_Reports__2018_to_Present.csv')
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
-mapa=pd.DataFrame()
+mapa=pd.DataFrame(df['Latitude'],df['Longitude'])
 mapa=mapa.dropna()
 st.map(mapa.astype(int))
